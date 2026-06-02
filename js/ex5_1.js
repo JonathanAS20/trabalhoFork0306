49fda677ab80dd1991c86ecc2f0977303dbdc362
Aluno <e023aluno@outlook.com>
2026-06-02 20:18:44 -0300

    Coloquei especialidades, e preferencial

--- js/ex5_1.js
+++ js/ex5_1.js
@@ -1,42 +1,76 @@
-var pacientes = [];       // declara vetor global
+var pacientes = [];
+var preferenciais = [];
 var urgencias = [];
 
 function atualizarLista() {
+
   var outLista = document.getElementById("outLista");
   var lista = "";
 
   if (urgencias.length > 0) {
+
     lista += "Urgências:\n";
 
     for (var i = 0; i < urgencias.length; i++) {
-      lista += (i + 1) + ". " + urgencias[i] + " - URGÊNCIA\n";
+
+      lista += (i + 1) + ". " +
+        urgencias[i] +
+        " - URGÊNCIA\n";
+    }
+
+    lista += "\n";
+  }
+
+  if (preferenciais.length > 0) {
+
+    lista += "Preferenciais:\n";
+
+    for (var i = 0; i < preferenciais.length; i++) {
+
+      lista += (i + 1) + ". " +
+        preferenciais[i] +
+        " - PREFERENCIAL\n";
     }
 
     lista += "\n";
   }
 
   if (pacientes.length > 0) {
+
     lista += "Fila Normal:\n";
 
     for (var i = 0; i < pacientes.length; i++) {
-      lista += (i + 1) + ". " + pacientes[i] + "\n";
+
+      lista += (i + 1) + ". " +
+        pacientes[i] +
+        "\n";
     }
   }
 
   outLista.textContent = lista;
 }
 
+// ADICIONAR PACIENTE NORMAL
 function adicionarPaciente() {
-  var inPaciente = document.getElementById("inPaciente");
+
+  var inPaciente =
+    document.getElementById("inPaciente");
+
+  var especialidade =
+    document.getElementById("inEspecialidade");
+
   var nome = inPaciente.value;
 
   if (nome == "") {
+
     alert("Informe o nome do paciente");
     inPaciente.focus();
     return;
   }
 
-  pacientes.push(nome);
+  pacientes.push(
+    nome + " - " + especialidade.value
+  );
 
   atualizarLista();
 
@@ -44,21 +78,71 @@ function adicionarPaciente() {
   inPaciente.focus();
 }
 
-var btAdicionar = document.getElementById("btAdicionar");
-btAdicionar.addEventListener("click", adicionarPaciente);
+var btAdicionar =
+  document.getElementById("btAdicionar");
 
+btAdicionar.addEventListener(
+  "click",
+  adicionarPaciente
+);
 
+// ADICIONAR PREFERENCIAL
+function adicionarPreferencial() {
+
+  var inPaciente =
+    document.getElementById("inPaciente");
+
+  var especialidade =
+    document.getElementById("inEspecialidade");
+
+  var nome = inPaciente.value;
+
+  if (nome == "") {
+
+    alert("Informe o nome do paciente");
+    inPaciente.focus();
+    return;
+  }
+
+  preferenciais.push(
+    nome + " - " + especialidade.value
+  );
+
+  atualizarLista();
+
+  inPaciente.value = "";
+  inPaciente.focus();
+}
+
+var btPreferencial =
+  document.getElementById("btPreferencial");
+
+btPreferencial.addEventListener(
+  "click",
+  adicionarPreferencial
+);
+
+// ADICIONAR URGÊNCIA
 function adicionarUrgencia() {
-  var inPaciente = document.getElementById("inPaciente");
+
+  var inPaciente =
+    document.getElementById("inPaciente");
+
+  var especialidade =
+    document.getElementById("inEspecialidade");
+
   var nome = inPaciente.value;
 
   if (nome == "") {
+
     alert("Informe o nome do paciente");
     inPaciente.focus();
     return;
   }
 
-  urgencias.push(nome);
+  urgencias.push(
+    nome + " - " + especialidade.value
+  );
 
   atualizarLista();
 
@@ -66,32 +150,61 @@ function adicionarUrgencia() {
   inPaciente.focus();
 }
 
-var btUrgencia = document.getElementById("btUrgencia");
-btUrgencia.addEventListener("click", adicionarUrgencia);
+var btUrgencia =
+  document.getElementById("btUrgencia");
 
+btUrgencia.addEventListener(
+  "click",
+  adicionarUrgencia
+);
 
+// ATENDER PACIENTE
 function atenderPaciente() {
-  var inPaciente = document.getElementById("inPaciente");
 
-  if (urgencias.length == 0 && pacientes.length == 0) {
-    alert("Não há pacientes na lista de espera");
+  var inPaciente =
+    document.getElementById("inPaciente");
+
+  if (
+    urgencias.length == 0 &&
+    preferenciais.length == 0 &&
+    pacientes.length == 0
+  ) {
+
+    alert(
+      "Não há pacientes na lista de espera"
+    );
+
     inPaciente.focus();
     return;
   }
 
-  var outAtendimento = document.getElementById("outAtendimento");
   var atender;
 
   if (urgencias.length > 0) {
+
     atender = urgencias.shift();
+
+  } else if (preferenciais.length > 0) {
+
+    atender = preferenciais.shift();
+
   } else {
+
     atender = pacientes.shift();
   }
+
+  var outAtendimento =
+    document.getElementById("outAtendimento");
 
   outAtendimento.textContent = atender;
 
   atualizarLista();
 }
 
-var btAtender = document.getElementById("btAtender");
-btAtender.addEventListener("click", atenderPaciente);
+var btAtender =
+  document.getElementById("btAtender");
+
+btAtender.addEventListener(
+  "click",
+  atenderPaciente
+);
